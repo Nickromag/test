fd788e8b7813b6e55c632962f703a7963c45f800
Nickromag <maksutov.maxim@gmail.com>
2023-07-11 18:23:36 +0500

    delete

--- commands/setAndEnter.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    command: function(selector, word) {
-        return this
-            .waitForElementVisible(selector)
-            .setValue(selector, [word, browser.Keys.ENTER]);
-    }
-};
